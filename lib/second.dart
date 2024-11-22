6db958c1b644e888c0748b3843e817aa56676609
asp <asp@aptech.com>
2024-11-22 10:59:14 +0500

    pyramit

---
+++ lib/second.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main(){
+
+
+// for(int i=0;i<=10;i++){
+
+//     print("");
+  
+//   for(int j=0;j<=i;j++){
+//     stdout.write(" *");
+ 
+  
+
+//   }
+
+// }
+for(int i=10;i>=0;i--){
+
+    print("");
+  
+  for(int j=i;j>0;j--){
+    stdout.write(" *");
+ 
+  
+
+  }
+
+}
+}
+
